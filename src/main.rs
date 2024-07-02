2657f14b955d0beea16618f7a7c4536dad39d99b
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2024-07-02 08:48:43 -0500

    try adding zstd compilation

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ use chrono::prelude::*;
 fn main() {
     let dt = Utc::now();
 
+    zstd::stream::copy_decode(std::io::stdin(), std::io::stdout()).unwrap();
+
     println!("Hello, world2! {}", dt);
 }
