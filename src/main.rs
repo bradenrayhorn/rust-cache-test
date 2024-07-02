a96d37a172ebaebdec56c23fbaf079d04f7a7fa4
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2024-07-02 08:52:16 -0500

    try rebuild again

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use chrono::prelude::*;
 fn main() {
     let dt = Utc::now();
 
-    zstd::stream::copy_decode(std::io::stdin(), std::io::stdout()).unwrap();
+    zstd::stream::copy_decode(std::io::stdin(), std::io::stdout()).expect("perfect");
 
-    println!("Hello, world2! {}", dt);
+    println!("Hello, world3! {}", dt);
 }
